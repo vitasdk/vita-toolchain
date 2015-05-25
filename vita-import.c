40f82d13d280b18cce35837ca7ad919215d65dc7
Sergi Granell <xerpi.g.12@gmail.com>
2015-05-25 23:05:43 +0200

    Changed modules<->libs Improved output (multiple files by module)

--- vita-import.c
+++ vita-import.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-vita_imports_t *vita_imports_new(int n_modules)
+vita_imports_t *vita_imports_new(int n_libs)
 {
 	vita_imports_t *imp = malloc(sizeof(*imp));
 	if (imp == NULL)
 		return NULL;
 
-	imp->modules = malloc(n_modules * sizeof(*imp->modules));
-	memset(imp->modules, 0, n_modules * sizeof(*imp->modules));
-	imp->n_modules = n_modules;
+	imp->n_libs = n_libs;
+
+	imp->libs = malloc(n_libs * sizeof(*imp->libs));
+	memset(imp->libs, 0, n_libs * sizeof(*imp->libs));
 
 	return imp;
 }
@@ -19,14 +20,31 @@ void vita_imports_free(vita_imports_t *imp)
 {
 	if (imp) {
 		int i;
-		for (i = 0; i < imp->n_modules; i++) {
-			vita_imports_module_free(imp->modules[i]);
+		for (i = 0; i < imp->n_libs; i++) {
+			vita_imports_lib_free(imp->libs[i]);
 		}
 		free(imp);
 	}
 }
 
-vita_imports_module_t *vita_imports_module_new(const char *name, uint32_t NID, int n_libs)
+vita_imports_lib_t *vita_imports_lib_new(const char *name, uint32_t NID, int n_modules)
+{
+	vita_imports_lib_t *lib = malloc(sizeof(*lib));
+	if (lib == NULL)
+		return NULL;
+
+	lib->name = strdup(name);
+	lib->NID = NID;
+	lib->n_modules = n_modules;
+
+	lib->modules = malloc(n_modules * sizeof(*lib->modules));
+	memset(lib->modules, 0, n_modules * sizeof(*lib->modules));
+
+	return lib;
+}
+
+
+vita_imports_module_t *vita_imports_module_new(const char *name, uint32_t NID, int n_functions, int n_variables)
 {
 	vita_imports_module_t *mod = malloc(sizeof(*mod));
 	if (mod == NULL)
@@ -34,9 +52,14 @@ vita_imports_module_t *vita_imports_module_new(const char *name, uint32_t NID, i
 
 	mod->name = strdup(name);
 	mod->NID = NID;
-	mod->n_libs = n_libs;
-	mod->libs = malloc(n_libs * sizeof(*mod->libs));
-	memset(mod->libs, 0, n_libs * sizeof(*mod->libs));
+	mod->n_functions = n_functions;
+	mod->n_variables = n_variables;
+
+	mod->functions = malloc(n_functions * sizeof(*mod->functions));
+	memset(mod->functions, 0, n_functions * sizeof(*mod->functions));
+
+	mod->variables = malloc(n_variables * sizeof(*mod->variables));
+	memset(mod->variables, 0, n_variables * sizeof(*mod->variables));
 
 	return mod;
 }
@@ -45,44 +68,24 @@ void vita_imports_module_free(vita_imports_module_t *mod)
 {
 	if (mod) {
 		int i;
-		for (i = 0; i < mod->n_libs; i++) {
-			vita_imports_lib_free(mod->libs[i]);
+		for (i = 0; i < mod->n_variables; i++) {
+			vita_imports_stub_free(mod->variables[i]);
+		}
+		for (i = 0; i < mod->n_functions; i++) {
+			vita_imports_stub_free(mod->functions[i]);
 		}
 		free(mod->name);
 		free(mod);
 	}
 }
 
-vita_imports_lib_t *vita_imports_lib_new(const char *name, uint32_t NID, int n_functions, int n_variables)
-{
-	vita_imports_lib_t *lib = malloc(sizeof(*lib));
-	if (lib == NULL)
-		return NULL;
-
-	lib->name = strdup(name);
-	lib->NID = NID;
-
-	lib->functions = malloc(n_functions * sizeof(*lib->functions));
-	memset(lib->functions, 0, n_functions * sizeof(*lib->functions));
-
-	lib->variables = malloc(n_variables * sizeof(*lib->variables));
-	memset(lib->variables, 0, n_variables * sizeof(*lib->variables));
-
-	lib->n_functions = n_functions;
-	lib->n_variables = n_variables;
-
-	return lib;
-}
 
 void vita_imports_lib_free(vita_imports_lib_t *lib)
 {
 	if (lib) {
 		int i;
-		for (i = 0; i < lib->n_variables; i++) {
-			vita_imports_stub_free(lib->variables[i]);
-		}
-		for (i = 0; i < lib->n_functions; i++) {
-			vita_imports_stub_free(lib->functions[i]);
+		for (i = 0; i < lib->n_modules; i++) {
+			vita_imports_module_free(lib->modules[i]);
 		}
 		free(lib->name);
 		free(lib);
--- vita-import.h
+++ vita-import.h
@@ -15,29 +15,29 @@ typedef struct {
 	vita_imports_stub_t **variables;
 	int n_functions;
 	int n_variables;
-} vita_imports_lib_t;
+} vita_imports_module_t;
 
 typedef struct {
 	char *name;
 	uint32_t NID;
-	vita_imports_lib_t **libs;
-	int n_libs;
-} vita_imports_module_t;
-
-typedef struct {
 	vita_imports_module_t **modules;
 	int n_modules;
+} vita_imports_lib_t;
+
+typedef struct {
+	vita_imports_lib_t **libs;
+	int n_libs;
 } vita_imports_t;
 
 
-vita_imports_t *vita_imports_new(int n_modules);
+vita_imports_t *vita_imports_new(int n_libs);
 void vita_imports_free(vita_imports_t *imp);
 
-vita_imports_module_t *vita_imports_module_new(const char *name, uint32_t NID, int n_libs);
-void vita_imports_module_free(vita_imports_module_t *mod);
-
-vita_imports_lib_t *vita_imports_lib_new(const char *name, uint32_t NID, int n_functions, int n_variables);
+vita_imports_lib_t *vita_imports_lib_new(const char *name, uint32_t NID, int n_modules);
 void vita_imports_lib_free(vita_imports_lib_t *lib);
+
+vita_imports_module_t *vita_imports_module_new(const char *name, uint32_t NID, int n_functions, int n_variables);
+void vita_imports_module_free(vita_imports_module_t *mod);
 
 vita_imports_stub_t *vita_imports_stub_new(const char *name, uint32_t NID);
 void vita_imports_stub_free(vita_imports_stub_t *stub);
