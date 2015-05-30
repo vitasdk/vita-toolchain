6c602702bbb9284e6013d7e9732df502a2279e0e
Danielle Church <dani.church@gmail.com>
2015-05-30 00:22:27 -0400

    Altering JSON DB format; now using objects rather than arrays

--- vita-import-parse.c
+++ vita-import-parse.c
@@ -27,8 +27,9 @@ vita_imports_t *vita_imports_load(const char *filename, int verbose)
 
 vita_imports_t *vita_imports_loads(const char *text, int verbose)
 {
-	json_t *libs;
+	json_t *libs, *lib_data;
 	json_error_t error;
+	const char *lib_name, *mod_name, *target_name;
 
 	libs = json_loads(text, 0, &error);
 	if (libs == NULL) {
@@ -36,179 +37,135 @@ vita_imports_t *vita_imports_loads(const char *text, int verbose)
 		return NULL;
 	}
 
-	if (!json_is_array(libs)) {
-		fprintf(stderr, "error: modules is not an array\n");
+	if (!json_is_object(libs)) {
+		fprintf(stderr, "error: modules is not an object\n");
 		json_decref(libs);
 		return NULL;
 	}
 
-	vita_imports_t *imports = vita_imports_new(json_array_size(libs));
-
+	vita_imports_t *imports = vita_imports_new(json_object_size(libs));
 	int i, j, k;
-	for (i = 0; i < json_array_size(libs); i++) {
-		json_t *data, *name, *nid, *modules;
 
-		data = json_array_get(libs, i);
+	i = -1;
+	json_object_foreach(libs, lib_name, lib_data) {
+		json_t *nid, *modules, *mod_data;
 
-		if (!json_is_object(data)) {
-			fprintf(stderr, "error: %d is not an object\n", i + 1);
+		i++;
+
+		if (!json_is_object(lib_data)) {
+			fprintf(stderr, "error: library %s is not an object\n", lib_name);
 			json_decref(libs);
 			return NULL;
 		}
 
-		name = json_object_get(data, "name");
-		if (!json_is_string(name)) {
-			fprintf(stderr, "error: name %d: name is not a string\n", i + 1);
-			json_decref(libs);
-			return NULL;
-		}
-
-		nid = json_object_get(data, "nid");
+		nid = json_object_get(lib_data, "nid");
 		if (!json_is_integer(nid)) {
-			fprintf(stderr, "error: name %d: nid is not an integer\n", i + 1);
+			fprintf(stderr, "error: library %s: nid is not an integer\n", lib_name);
 			json_decref(libs);
 			return NULL;
 		}
 
-		modules = json_object_get(data, "modules");
-		if (!json_is_array(modules)) {
-			fprintf(stderr, "error: name %d: module is not an array\n", i + 1);
+		modules = json_object_get(lib_data, "modules");
+		if (!json_is_object(modules)) {
+			fprintf(stderr, "error: library %s: module is not an object\n", lib_name);
 			json_decref(libs);
 			return NULL;
 		}
 
 		imports->libs[i] = vita_imports_lib_new(
-			json_string_value(name),
-			json_integer_value(nid),
-			json_array_size(modules));
+				lib_name,
+				json_integer_value(nid),
+				json_object_size(modules));
 
-                if (verbose)
-			printf("Lib: %s\n", json_string_value(name));
+		if (verbose)
+			printf("Lib: %s\n", lib_name);
 
-		for (j = 0; j < json_array_size(modules); j++) {
-			json_t *data, *name, *nid, *kernel, *functions, *variables;
+		j = -1;
+		json_object_foreach(modules, mod_name, mod_data) {
+			json_t *nid, *kernel, *functions, *variables, *target_nid;
 
-			data = json_array_get(modules, j);
+			j++;
 
-			if (!json_is_object(data)) {
-				fprintf(stderr, "error: %d is not an object\n", i + 1);
+			if (!json_is_object(mod_data)) {
+				fprintf(stderr, "error: module %s is not an object\n", mod_name);
 				json_decref(libs);
 				return NULL;
 			}
 
-			name = json_object_get(data, "name");
-			if (!json_is_string(name)) {
-				fprintf(stderr, "error: name %d: name is not a string\n", i + 1);
-				json_decref(libs);
-				return NULL;
-			}
-
-			nid = json_object_get(data, "nid");
+			nid = json_object_get(mod_data, "nid");
 			if (!json_is_integer(nid)) {
-				fprintf(stderr, "error: name %d: nid is not an integer\n", i + 1);
+				fprintf(stderr, "error: module %s: nid is not an integer\n", mod_name);
 				json_decref(libs);
 				return NULL;
 			}
 
-			kernel = json_object_get(data, "kernel");
+			kernel = json_object_get(mod_data, "kernel");
 			if (!json_is_boolean(kernel)) {
-				fprintf(stderr, "error: name %d: kernel is not a boolean\n", i + 1);
+				fprintf(stderr, "error: module %s: kernel is not a boolean\n", mod_name);
 				json_decref(libs);
 				return NULL;
 			}
 
-			functions = json_object_get(data, "functions");
-			if (!json_is_array(functions)) {
-				fprintf(stderr, "error: name %d: functions is not an array\n", i + 1);
+			functions = json_object_get(mod_data, "functions");
+			if (!json_is_object(functions)) {
+				fprintf(stderr, "error: module %s: functions is not an array\n", mod_name);
 				json_decref(libs);
 				return NULL;
 			}
 
-			variables = json_object_get(data, "variables");
-			if (!json_is_array(variables)) {
-				fprintf(stderr, "error: name %d: variables is not an array\n", i + 1);
+			variables = json_object_get(mod_data, "variables");
+			if (!json_is_object(variables)) {
+				fprintf(stderr, "error: module %s: variables is not an array\n", mod_name);
 				json_decref(libs);
 				return NULL;
 			}
 
 			if (verbose)
-				printf("\tModule: %s\n", json_string_value(name));
+				printf("\tModule: %s\n", mod_name);
 
 			imports->libs[i]->modules[j] = vita_imports_module_new(
-				json_string_value(name),
-				json_integer_value(nid),
-				json_array_size(functions),
-				json_array_size(variables));
+					mod_name,
+					json_integer_value(nid),
+					json_object_size(functions),
+					json_object_size(variables));
 
-			for (k = 0; k < json_array_size(functions); k++) {
-				json_t *data, *name, *nid;
+			k = -1;
+			json_object_foreach(functions, target_name, target_nid) {
+				k++;
 
-				data = json_array_get(functions, k);
-
-				if (!json_is_object(data)) {
-					fprintf(stderr, "error: %d is not an object\n", i + 1);
-					json_decref(libs);
-					return NULL;
-				}
-
-				name = json_object_get(data, "name");
-				if (!json_is_string(name)) {
-					fprintf(stderr, "error: name %d: name is not a string\n", i + 1);
-					json_decref(libs);
-					return NULL;
-				}
-
-				nid = json_object_get(data, "nid");
-				if (!json_is_integer(nid)) {
-					fprintf(stderr, "error: name %d: nid is not an integer\n", i + 1);
+				if (!json_is_integer(target_nid)) {
+					fprintf(stderr, "error: function %s: nid is not an integer\n", target_name);
 					json_decref(libs);
 					return NULL;
 				}
 
 				if (verbose)
-					printf("\t\tFunction: %s\n", json_string_value(name));
+					printf("\t\tFunction: %s\n", target_name);
 
 				imports->libs[i]->modules[j]->functions[k] = vita_imports_stub_new(
-					json_string_value(name),
-					json_integer_value(nid));
+						target_name,
+						json_integer_value(target_nid));
 
 			}
 
-			for (k = 0; k < json_array_size(variables); k++) {
-				json_t *data, *name, *nid;
+			k = -1;
+			json_object_foreach(variables, target_name, target_nid) {
+				k++;
 
-				data = json_array_get(variables, k);
-
-				if (!json_is_object(data)) {
-					fprintf(stderr, "error: %d is not an object\n", i + 1);
-					json_decref(libs);
-					return NULL;
-				}
-
-				name = json_object_get(data, "name");
-				if (!json_is_string(name)) {
-					fprintf(stderr, "error: name %d: name is not a string\n", i + 1);
-					json_decref(libs);
-					return NULL;
-				}
-
-				nid = json_object_get(data, "nid");
-				if (!json_is_integer(nid)) {
-					fprintf(stderr, "error: name %d: nid is not an integer\n", i + 1);
+				if (!json_is_integer(target_nid)) {
+					fprintf(stderr, "error: variable %s: nid is not an integer\n", target_name);
 					json_decref(libs);
 					return NULL;
 				}
 
 				if (verbose)
-					printf("\t\tVariable: %s\n", json_string_value(name));
+					printf("\t\tVariable: %s\n", target_name);
 
 				imports->libs[i]->modules[j]->variables[k] = vita_imports_stub_new(
-					json_string_value(name),
-					json_integer_value(nid));
+						target_name,
+						json_integer_value(target_nid));
 
 			}
-
-
 
 		}
 	}
