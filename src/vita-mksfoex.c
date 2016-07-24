28ead9e6f44a1935e7bc6d97a30f36a0aac7a376
Ilya Zhuravlev <whatever@xyz.is>
2016-07-24 10:33:11 -0400

    Update the defaults

--- src/vita-mksfoex.c
+++ src/vita-mksfoex.c
@@ -51,7 +51,7 @@ struct EntryContainer
 
 struct EntryContainer g_defaults[] = {
 	{ "APP_VER", PSF_TYPE_STR, 0, "00.00" },
-	{ "ATTRIBUTE", PSF_TYPE_VAL, 0, NULL },
+	{ "ATTRIBUTE", PSF_TYPE_VAL, 0x8000, NULL },
 	{ "BOOT_FILE", PSF_TYPE_STR, 32, "" },
 	{ "CATEGORY", PSF_TYPE_STR, 0, "gd" },
 	{ "CONTENT_ID", PSF_TYPE_STR, 48, "" },
