3e28f963623bf5ecc6707bc08f1dda929edeea5e
Davee <davee@x-fusion.co.uk>
2016-08-06 16:31:39 +0100

    Improved CMake scripts for libzip

--- cmake/Modules/Findlibzip.cmake
+++ cmake/Modules/Findlibzip.cmake
@@ -4,11 +4,14 @@ pkg_check_modules(PC_libzip libzip>=1.1.3)
 find_path(libzip_INCLUDE_DIR zip.h
           HINTS ${PC_libzip_INCLUDEDIR} ${PC_libzip_INCLUDE_DIRS})
 
+find_path(libzip_CONFIG_INCLUDE_DIR zipconf.h
+          HINTS ${PC_libzip_INCLUDEDIR} ${PC_libzip_INCLUDE_DIRS})
+
 find_library(libzip_LIBRARY NAMES zip
              HINTS ${PC_libzip_LIBDIR} ${PC_libzip_LIBRARY_DIRS} )
 
 set(libzip_LIBRARIES ${libzip_LIBRARY})
-set(libzip_INCLUDE_DIRS ${libzip_INCLUDE_DIR})
+set(libzip_INCLUDE_DIRS ${libzip_INCLUDE_DIR} ${libzip_CONFIG_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(libzip DEFAULT_MSG
