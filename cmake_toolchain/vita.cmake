vitasdk/vita-toolchain

commit dadf354a5a1c3a30abe3935786c362cdd9faa27c

vita.cmake: avoid rebuilding unnecessarily vpk and eboot (#120)
Fix #116.

cmake_toolchain/vita.cmake
@@ -64,12 +64,12 @@ macro(vita_create_self target source)
     COMMENT "Converting to Sony ELF ${source}.velf" VERBATIM
   )
   separate_arguments(VITA_MAKE_FSELF_FLAGS)
-  add_custom_target(${target} ALL
+  add_custom_command(OUTPUT ${target}
     COMMAND ${VITA_MAKE_FSELF} ${VITA_MAKE_FSELF_FLAGS} ${source}.velf ${target}
     DEPENDS ${source}.velf
     COMMENT "Creating SELF ${target}"
   )
-  add_dependencies(${target} ${source})
+  add_custom_target(${target}_ ALL DEPENDS ${target})
 endmacro(vita_create_self)
 ##################################################
 
@@ -178,11 +178,11 @@ macro(vita_create_vpk target titleid eboot)
     COMMENT "Generating param.sfo for ${target}"
   )
   separate_arguments(VITA_PACK_VPK_FLAGS)
-  add_custom_target(${target} ALL
+  add_custom_command(OUTPUT ${target}
     COMMAND ${VITA_PACK_VPK} ${VITA_PACK_VPK_FLAGS} -s ${target}_param.sfo -b ${eboot} ${target}
-    DEPENDS ${target}_param.sfo
+    DEPENDS ${target}_param.sfo ${eboot}
     COMMENT "Building vpk ${target}"
   )
-  add_dependencies(${target} ${eboot})
+  add_custom_target(${target}_ ALL DEPENDS ${target})
 endmacro(vita_create_vpk)
 ##################################################

commit 77815b7518fa9029582416c38accf68c1058c89c

vita.cmake: Giva the same name as custom_command
this patch would recover the broken name since #120

this patch will print the warning about `circular dependency dropped`
but would work properly.

actually, we should give the right name to target (#129 still has wrong name)

follow the 2011 cmake newgroup thread & cmake issue
- https://cmake.org/pipermail/cmake/2011-March/043375.html
- https://gitlab.kitware.com/cmake/cmake/issues/18627

cmake_toolchain/vita.cmake
@@ -64,12 +64,12 @@ macro(vita_create_self target source)
     COMMENT "Converting to Sony ELF ${source}.velf" VERBATIM
   )
   separate_arguments(VITA_MAKE_FSELF_FLAGS)
-  add_custom_command(OUTPUT ${target}
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}
     COMMAND ${VITA_MAKE_FSELF} ${VITA_MAKE_FSELF_FLAGS} ${source}.velf ${target}
     DEPENDS ${source}.velf
     COMMENT "Creating SELF ${target}"
   )
-  add_custom_target(${target}_ ALL DEPENDS ${target})
+  add_custom_target(${target} ALL DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${target})
 endmacro(vita_create_self)
 ##################################################
 
@@ -179,11 +179,11 @@ macro(vita_create_vpk target titleid eboot)
     COMMENT "Generating param.sfo for ${target}"
   )
   separate_arguments(VITA_PACK_VPK_FLAGS)
-  add_custom_command(OUTPUT ${target}
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}
     COMMAND ${VITA_PACK_VPK} ${VITA_PACK_VPK_FLAGS} -s ${target}_param.sfo -b ${eboot} ${target}
     DEPENDS ${target}_param.sfo ${eboot} ${resources}
     COMMENT "Building vpk ${target}"
   )
-  add_custom_target(${target}_ ALL DEPENDS ${target})
+  add_custom_target(${target} ALL DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${target})
 endmacro(vita_create_vpk)
 ##################################################