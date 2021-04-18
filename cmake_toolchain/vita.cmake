c8161c7ab1b76a72d439ae152681abcac3d08d2f
Sunguk Lee <d3m3vilurr@gmail.com>
2021-04-19 02:55:16 +0900

    Revert "Revert "Fix big align velf(issues/177) (#178)""
    This reverts commit 271b8203c562a94135d9a86bb1a30a36ac7877ef.

--- cmake_toolchain/vita.cmake
+++ cmake_toolchain/vita.cmake
@@ -26,7 +26,8 @@ include(CMakeParseArguments)
 ##   vita_create_self(target source
 ##                    [CONFIG file]
 ##                    [UNCOMPRESSED]
-##                    [UNSAFE])
+##                    [UNSAFE]
+##                    [STRIPPED])
 ##
 ## @param target
 ##   A CMake custom target of this given name
@@ -44,13 +45,16 @@ macro(vita_create_self target source)
   set(VITA_ELF_CREATE_FLAGS "${VITA_ELF_CREATE_FLAGS}" CACHE STRING "vita-elf-create flags")
   set(VITA_MAKE_FSELF_FLAGS "${VITA_MAKE_FSELF_FLAGS}" CACHE STRING "vita-make-fself flags")
 
-  set(options UNCOMPRESSED UNSAFE)
+  set(options UNCOMPRESSED UNSAFE STRIPPED)
   set(oneValueArgs CONFIG)
   cmake_parse_arguments(vita_create_self "${options}" "${oneValueArgs}" "" ${ARGN})
 
   if(vita_create_self_CONFIG)
     get_filename_component(fconfig ${vita_create_self_CONFIG} ABSOLUTE)
     set(VITA_ELF_CREATE_FLAGS "${VITA_ELF_CREATE_FLAGS} -e ${fconfig}")
+  endif()
+  if(vita_create_self_STRIPPED)
+    set(VITA_ELF_CREATE_FLAGS "${VITA_ELF_CREATE_FLAGS} -s")
   endif()
   if(NOT vita_create_self_UNCOMPRESSED)
     set(VITA_MAKE_FSELF_FLAGS "${VITA_MAKE_FSELF_FLAGS} -c")
