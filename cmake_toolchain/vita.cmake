f313b9c17807251ca2c8fb7b69058a20d9507e23
Ivan Epifanov <isage.dna@gmail.com>
2021-10-07 18:03:11 +0300

    Use BYPRODUCTS on targets only. Rename targets to not clash with byproducts

--- cmake_toolchain/vita.cmake
+++ cmake_toolchain/vita.cmake
@@ -147,7 +147,6 @@ macro(vita_create_self target source)
     COMMAND ${VITA_ELF_CREATE} ${VITA_ELF_CREATE_FLAGS} ${sourcepath} ${CMAKE_CURRENT_BINARY_DIR}/${sourcefile}.velf
     DEPENDS ${sourcepath}
     COMMENT "Converting to Sony ELF ${sourcefile}.velf" VERBATIM
-    BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/${sourcefile}.velf
   )
 
   set(self_outfile ${CMAKE_CURRENT_BINARY_DIR}/${target}.out)
@@ -158,11 +157,10 @@ macro(vita_create_self target source)
     COMMAND ${VITA_MAKE_FSELF} ${VITA_MAKE_FSELF_FLAGS} ${CMAKE_CURRENT_BINARY_DIR}/${sourcefile}.velf ${self_outfile}
     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${sourcefile}.velf
     COMMENT "Creating SELF ${target}"
-    BYPRODUCTS ${self_outfile}
   )
 
   ## SELF target
-  add_custom_target(${target}
+  add_custom_target(${target}-self
     ALL
     DEPENDS ${self_outfile}
     COMMAND ${CMAKE_COMMAND} -E copy ${self_outfile} ${target}
@@ -170,7 +168,7 @@ macro(vita_create_self target source)
   )
 
   if(TARGET ${source})
-    add_dependencies(${target} ${source})
+    add_dependencies(${target}-self ${source})
   endif()
 endmacro(vita_create_self)
 ##################################################
@@ -321,6 +319,8 @@ macro(vita_create_vpk target titleid eboot)
   ## check eboot for being a target, otherwise it is a file path
   if(TARGET ${eboot})
     set(sourcepath ${CMAKE_CURRENT_BINARY_DIR}/${eboot})
+  elseif(TARGET ${eboot}-self)
+    set(sourcepath ${CMAKE_CURRENT_BINARY_DIR}/${eboot})
   else()
     set(sourcepath ${eboot})
   endif()
@@ -333,7 +333,6 @@ macro(vita_create_vpk target titleid eboot)
     DEPENDS ${sourcepath}
     COMMENT "Generating param.sfo for ${target}"
     VERBATIM
-    BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/${target}_param.sfo
   )
 
   set(vpk_outfile ${CMAKE_CURRENT_BINARY_DIR}/${target}.out)
@@ -346,11 +345,10 @@ macro(vita_create_vpk target titleid eboot)
     DEPENDS ${sourcepath}
     DEPENDS ${resources}
     COMMENT "Building vpk ${target}"
-    BYPRODUCTS ${vpk_outfile}
   )
 
   ## VPK target
-  add_custom_target(${target}
+  add_custom_target(${target}-vpk
     ALL
     DEPENDS ${vpk_outfile}
     COMMAND ${CMAKE_COMMAND} -E copy ${vpk_outfile} ${target}
@@ -358,7 +356,7 @@ macro(vita_create_vpk target titleid eboot)
   )
 
   if(TARGET ${eboot})
-    add_dependencies(${target} ${eboot})
+    add_dependencies(${target}-vpk ${eboot})
   endif()
 endmacro(vita_create_vpk)
 ##################################################
